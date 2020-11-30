e396128872ec8735cb1bb467551ccee08b67b1a6
parsecmonkey <60334704+parsecmonkey@users.noreply.github.com>
2020-11-30 23:24:42 +0900

    Update Main.java

--- code1/Main.java
+++ code1/Main.java
@@ -1,1 +1,17 @@
+public class Main {
+    public static void main(String[] args){
 
+        final double PI = 3.14; // 定数
+        int pie = 5;
+
+        System.out.println("半径" + pie + "cmのパイの面積は，");
+        System.out.println(pie * pie * PI);
+        System.out.println("パイの半径を倍にします");
+
+        // エラーになる PI = 10;
+
+        System.out.println("半径" + pie + "cmのパイの面積は，");
+        System.out.println(pie * pie * PI);
+
+    }
+}
